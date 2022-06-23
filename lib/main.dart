3c8d3698c71ac0ddefbc4f89e4109b02cbc8979b
esrael23 <93195740+esrael23@users.noreply.github.com>
2022-06-23 13:59:50 -0700

    chenge mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: qrscan(),
+      home: mainpage(),
 
       // home: mainpage(),
     );
